2110b7b83aab7532d07921d497c510891a6fc4be
Сулейман Хасанов <macbook@MacBook-Air.local>
2023-02-12 14:39:31 +0500

    Урок №1: Вывод данных в консоль.

---
+++ src/lesson_1/lesson_1.java
@@ -0,0 +1,17 @@
+package lesson_1;
+/*Урок №1: Вывод данных в консоль. Будет выведенна в консоль Рубаи Омара Хайяма*/
+
+// Для вывода в консоль данных используется "System.out.print(Выводимые данные)"
+
+public class lesson_1 {
+    public static void main(String[] args){
+        System.out.println(" ");
+        System.out.println("Омара Хайам  – о жизни, счастье и любви:");
+        System.out.println("                  ***");
+        System.out.println("Бренность мира узрев, горевать погоди!");
+        System.out.println("Верь: недаром колотится сердце в груди.");
+        System.out.println("Не горюй о минувшем: что было, то сплыло.");
+        System.out.println("Не горюй о грядущем: туман впереди...");
+        System.out.println("                            (Омар Хайям)");
+    }
+}
